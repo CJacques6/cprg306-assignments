16265ba827d922704575170001c19c60b2b8f31f
CJacques6 <151947212+CJacques6@users.noreply.github.com>
2025-09-09 14:21:16 -0600

    finished week 2 assignment

--- app/page.js
+++ app/page.js
@@ -4,9 +4,9 @@ export default function Home(){
 
   return(
     <main>
-      <h1>CPRG306 Assignments</h1>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <ul>
-        <li><Link href="./week-2">Week 2 Assignment</Link></li>
+        <li><Link href="./week-2" className="text-cyan-500 underline hover:text-cyan-300">Week 2 Assignment</Link></li>
       </ul>
     </main>
   );
---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+
+export default function StudentInfo(){
+
+
+
+
+    return(
+        <div>
+        <h1>Cooper Jacques</h1>
+        <p><Link href="https://github.com/CJacques6/cprg306-assignments" className="text-cyan-500 underline hover:text-cyan-300">Github</Link></p>
+        </div>
+    );
+}
